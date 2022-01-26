085986dcc70259e02fa8da219878a5136e92fc7e
David <davidgdalevich7@gmail.com>
2022-01-26 16:32:53 +0200

    bundling fun: removed webpack, made esm the default `yoffee.min.js`, and fixed sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
-import { terser } from 'rollup-plugin-terser';
-import commonjs from 'rollup-plugin-commonjs';
-import { uglify } from 'rollup-plugin-uglify';
+import {terser} from 'rollup-plugin-terser';
+import {uglify} from 'rollup-plugin-uglify';
 import strip from '@rollup/plugin-strip';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -8,30 +7,29 @@ const production = !process.env.ROLLUP_WATCH;
 
 export default {
     input: './src/yoffee.js',
-    output: [{
-        file: './dist/yoffee.iife.js',
-        format: 'iife',
-        name: 'iife'
-    },
-    {
-        file: './dist/yoffee.esm.js',
-        format: 'esm',
-        name: 'esm'
-    },
-    {
-        file: './dist/yoffee.umd.js',
-        format: 'umd',
-        name: 'umd'
-    },
-    {
-        file: './dist/yoffee.cjs.js',
-        format: 'cjs',
-        name: 'cjs'
-    },
-    {
-        file: './dist/yoffee.js.map',
-        sourcemap: 'inline'
-    }],
+    output: [
+        {
+            file: './dist/yoffee.iife.js',
+            format: 'iife',
+            name: 'iife'
+        },
+        {
+            file: './dist/yoffee.min.js',
+            format: 'esm',
+            name: 'esm',
+            sourcemap: true
+        },
+        {
+            file: './dist/yoffee.umd.js',
+            format: 'umd',
+            name: 'umd'
+        },
+        {
+            file: './dist/yoffee.cjs.js',
+            format: 'cjs',
+            name: 'cjs'
+        }
+    ],
     plugins: [
         strip(),
         production && terser({
@@ -39,11 +37,10 @@ export default {
                 comments: false
             },
             compress: true,
-                mangle: {
-                    reserved: ["yoffee", "exports", "replaceWith"],
-                } 
+            mangle: {
+                reserved: ["yoffee", "exports", "replaceWith"],
+            }
         }),
-        commonjs(),
         uglify()
     ]
 }
